496c667b4e09dbb84158aa7fbe625e1bc8d67c38
Sharmina Akhtar <sakht318@gmail.com>
2018-03-04 11:02:53 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where("rating = 10").name
+    Show.where("rating = 10").order(:name)
   end
 
   def self.lowest_rating
