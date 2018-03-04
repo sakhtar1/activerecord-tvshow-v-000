a13e2807cf2f36811cb534f65a2ca8780cb4dccc
Sharmina Akhtar <sakht318@gmail.com>
2018-03-04 10:57:17 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.maximum(:rating).order(name: Limit)
+    Show.maximum(:rating).order(:name)
   end
 
   def self.lowest_rating
