2d56ff3cfcf9eb3e3c13825b6cf4d6b2af556998
Sharmina Akhtar <sakht318@gmail.com>
2018-03-04 10:49:56 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,27 @@
 class Show < ActiveRecord::Base
+
+  def highest_rating
+    Show.maximum(:rating)
+  end
+
+  def most_popular_show
+
+  end
+
+  def lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def least_popular_show
+
+  end
+
+  def ratings_sum
+    Show.sum(:rating)
+  end
+
+  def popular_shows
+    Song.where("rating > 5").order(rating: :asc)
+  end
+
 end
