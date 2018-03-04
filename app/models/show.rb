d93ab0152c5d9af554cc102aabb44d1cb817e502
Sharmina Akhtar <sakht318@gmail.com>
2018-03-04 11:11:26 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.where(:name).minimum(:rating)
+    #Show.where(:name).minimum(:rating)
   end
 
   def self.ratings_sum
